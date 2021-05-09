ead5db2e7b56400618954a9e0e2bcc918d74e131
Valetine <valentinnsh@gmail.com>
2021-05-09 21:16:48 +0300

    necessary changes

--- zc/hub_zc.c
+++ zc/hub_zc.c
@@ -1,6 +1,24 @@
-/*  Copyright (c)  DSR Corporation, Denver CO, USA.
-PURPOSE: ZB Simple output device
-*/
+/* ZBOSS Zigbee software protocol stack
+ *
+ * Copyright (c) 2012-2020 DSR Corporation, Denver CO, USA.
+ * www.dsr-zboss.com
+ * www.dsr-corporation.com
+ * All rights reserved.
+ *
+ * This is unpublished proprietary source code of DSR Corporation
+ * The copyright notice does not evidence any actual or intended
+ * publication of such source code.
+ *
+ * ZBOSS is a registered trademark of Data Storage Research LLC d/b/a DSR
+ * Corporation
+ *
+ * Commercial Usage
+ * Licensees holding valid DSR Commercial licenses may use
+ * this file in accordance with the DSR Commercial License
+ * Agreement provided with the Software or, alternatively, in accordance
+ * with the terms contained in a written agreement between you and
+ * DSR.
+ */
 /* ![mem_config_max] */
 #define ZB_TRACE_FILE_ID 1196
 #include "zboss_api.h"
